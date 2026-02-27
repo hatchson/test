334a657d900cfc979bf1d6d216f4a5fe3d452252
Nxium <deathgaming1886@gmail.com>
2026-02-27 17:10:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import requests
+import json
+
+def get_public_ip():
+    response = requests.get("https://api.ipify.org/?format=json")
+    return response.json()["ip"] 
+
+message = get_public_ip()
+def send_discord_message(message):
+    payload = {
+        "content": message
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(WEBHOOK_URL, data=json.dumps(payload), headers=headers)
+
+send_discord_message()
